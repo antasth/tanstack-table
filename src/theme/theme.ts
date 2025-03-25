843a2fe1fa82e90f070296bca806adb1e63a0dc8
antasth <antasth@mail.ru>
2025-03-25 17:51:37 +0300

    feat: add default styles

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,6 +1,31 @@
 import { createSystem, defineConfig } from '@chakra-ui/react'
 
 const config = defineConfig({
+  globalCss: {
+    'html, body': {
+      backgroundColor: '#3e626b',
+    },
+    '.table': {
+      border: '1px solid #4242424d',
+      borderRadius: '10px',
+    },
+    '.table-row': {
+      display: 'flex',
+      gap: '10px',
+    },
+    '.table-cell': {
+      position: 'relative',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '0.5rem',
+      fontWeight: 'bold',
+      fontSize: 'xs',
+      textWrap: 'nowrap',
+      textTransform: 'uppercase',
+      textAlign: 'center',
+    },
+  },
   theme: {
     tokens: {
       colors: {},
