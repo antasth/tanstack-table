c586527539299d83cec35c8be7ca2d987afef4ae
antasth <antasth@mail.ru>
2025-03-27 19:02:24 +0300

    feat: extend tanstack TableMeta interface

---
+++ src/types/tanstack-table.d.ts
@@ -0,0 +1,11 @@
+import '@tanstack/react-table'
+
+declare module '@tanstack/react-table' {
+  interface TableMeta<TData extends RowData> {
+    updateUsers: <TKey extends keyof TData>(
+      rowIndex: number,
+      columnId: TKey,
+      value: TData[TKey]
+    ) => void
+  }
+}
