0d2db5e0271af82682cd70f16bbf119b057446b7
antasth <antasth@mail.ru>
2025-03-28 21:46:22 +0300

    feat: add EditCell input styles

--- src/components/EditCell.tsx
+++ src/components/EditCell.tsx
@@ -1,20 +1,13 @@
+import { ICellProps } from '@/types/types'
 import { Input } from '@chakra-ui/react'
-import { CellContext } from '@tanstack/react-table'
 import { useEffect, useState } from 'react'
-
-interface ICustomCellProps<TData, TValue> {
-  getValue: CellContext<TData, string>['getValue']
-  row: CellContext<TData, TValue>['row']
-  column: CellContext<TData, TValue>['column']
-  table: CellContext<TData, TValue>['table']
-}
 
 const EditCell = <TData, TValue>({
   getValue,
   row,
   column,
   table,
-}: ICustomCellProps<TData, TValue>) => {
+}: ICellProps<TData, TValue>) => {
   const initialValue = getValue()
   const [value, setValue] = useState(initialValue)
 
@@ -37,13 +30,18 @@ const EditCell = <TData, TValue>({
       value={String(value)}
       onChange={handleInputChange}
       onBlur={handleBlur}
-      width="100%"
+      variant="outline"
+      padding="2px"
       border="none"
       textAlign="center"
-      padding="5px"
       fontSize="{fontSizes.lg}"
-      backgroundColor="{colors.primary}"
+      backgroundColor="{colors.surface}"
       textOverflow="ellipsis"
+      _focus={{
+        outline: '2px solid',
+        outlineColor: 'accent',
+        boxShadow: 'none',
+      }}
     ></Input>
   )
 }
