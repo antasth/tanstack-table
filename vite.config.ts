c1894233e08034057981133e768c1313a3d0b930
antasth <antasth@mail.ru>
2025-04-10 14:20:18 +0300

    refactor: add deploy commands

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  base: '/tanstack-table/',
   plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: {
